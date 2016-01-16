c21ff85169a8028b5dfa292c12792893a3b005e5
Jordan Wimberley <jordan.wimberley@gmail.com>
2016-01-16 12:16:09 -0800

    Updated exercise 7.

--- Week1/week1-7.py
+++ Week1/week1-7.py
@@ -10,5 +10,8 @@ with open("yaml_test.yml") as f:
 with open("json_test.json") as f:
 	json_file = json.load(f)
 
+print("Reading YAML file")
 print(pp(yaml_file))
+print("")
+print("Reading JSON file")
 print(pp(json_file))
