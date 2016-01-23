e130c102efd349e4c5c650b1ea9b1d4cefb20d32
Jordan Wimberley <jordan.wimberley@gmail.com>
2016-01-23 13:45:56 -0800

    Exercise 2 file.

---
+++ class2/exercise2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from telnetlib import Telnet
+import getpass
+import time
+
+username = 'pyclass'
+password = getpass.getpass()
+host = '50.76.53.27'
+
+def getIpInterfaces(host,user,password):
+    timeout = 6
+    port = 23
+    remote_conn = Telnet(host,port,timeout)
+    remote_conn.read_until(b"sername:")
+    remote_conn.write(username.encode('ascii') + b"\n")
+    remote_conn.read_until(b"assword:")
+    remote_conn.write(password.encode('ascii') + b"\n")
+    remote_conn.write(b"show ip int brief" + b"\n")
+    time.sleep(2)
+    output = remote_conn.read_very_eager()
+    remote_conn.write(b"exit" + b"\b")
+    print(output.decode() + "\n")
+
+getIpInterfaces(host,username,password)
+
