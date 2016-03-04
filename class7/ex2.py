8d8aff373182965f0670ab7744a909645d8cf443
Jordan Wimberley <jordan.wimberley@gmail.com>
2016-03-03 23:43:33 -0600

    Ex2 changes.

--- class7/ex2.py
+++ class7/ex2.py
@@ -20,26 +20,40 @@ def check_vlan(config,vlan_id):
 def create_vlan(connection,vlan_id,vlan_name):
     connection.enable("vlan %s" % vlan_id, "name %s" % vlan_name)
 
+def remove_vlan(connection,vlan_id,vlan_name):
+    connection.enable("no vlan %s" % vlan_id)
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--name", type=str, default="")
     parser.add_argument("vlan", type=str)
     parser.add_argument("--remove", action="store_true", default=False)
     args = parser.parse_args()
+    print("Checking for VLAN...")
+
+    print(args.name)
+    print(args.vlan)
+    print(args.remove)
 
     vlan_id = args.vlan
     vlan_name = args.name
     pynet_sw2 = pyeapi.connect_to('pynet-sw2')
     config = get_config(pynet_sw2)
 
-    print("Checking for VLAN...")
-    status = check_vlan(config,vlan_id)
-    if status = False:
-        print("No VLAN found.")
-        #create_vlan(pynet_sw2,vlan_id, vlan_name)
-        #print("\nVLAN has been created.")
+    vlan_status = check_vlan(config,vlan_id)
+    if args.remove == False:
+        if vlan_status = False:
+            print("No VLAN found.")
+            #create_vlan(pynet_sw2,vlan_id,vlan_name)
+            print("\nVLAN has been created.")
+        else:
+            print("This VLAN is currently in use.")
     else:
-        print("This VLAN is currently in use.")
+        if vlan_status = False:
+            print("No VLAN found.")
+        else:
+            #remove_vlan(pynet_sw2,vlan_id,vlan_name)
+            print("\nVLAN has been removed.")            
 
 if __name__ == "__main__":
     main()
