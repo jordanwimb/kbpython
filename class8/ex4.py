62102820db45908f40274fcec97c58a335377cc5
Jordan Wimberley <jordan.wimberley@gmail.com>
2016-03-09 22:36:06 -0600

    Ex4 changes.

---
+++ class8/ex4.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import django
+from net_system.models import NetworkDevice, Credentials
+
+devices = NetworkDevice.objects.all()
+creds = Credentials.objects.all()
+
+def main():
+    dev_name = raw_input("Enter device to delete or 'q' to quit: ")
+    
+    while not dev_name == 'q':
+        dev_name.delete()
+        print("%s has been deleted." % dev_name)
+
+    else:
+        print("Quitting.")
+
+if __name__ == '__main__':
+    main()
